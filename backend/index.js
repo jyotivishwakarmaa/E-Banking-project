eb2c3a50f043b46c67b1057db2f535fcb2e67fc1
Jyoti Vishwakarma <vishwakarmajyoti65@gmail.com>
2025-03-24 17:40:10 +0530

    dfghjkjhgf

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,38 @@
-const express=require('express')
+const express = require("express");
 
-const app=express()
-const route=require('./router/router')
-const depoRoute=require('./router/depositRouter')
+const app = express();
+const route = require("./router/router");
+const depoRoute = require("./router/depositRouter");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const cors=require('cors')
-const mongoose=require('mongoose')
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const bodyParser=require('body-parser')
+const bodyParser = require("body-parser");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use('/upload', express.static('upload'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/upload", express.static("upload"));
 
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("DB CONNECTED SUCCESsFULLY");
+  })
+  .catch(() => {
+    console.log("mot connected");
+  });
 
-mongoose.connect(process.env.MONGODB).then(()=>{
-    console.log('DB CONNECTED SUCCESsFULLY')
-})
+app.use("/bank", route);
+app.use("/bank", depoRoute);
 
-
-
-
-
-app.use('/bank',route)
-app.use('/bank',depoRoute)
-
-
-const port=process.env.PORT || 8000
-app.listen(port,()=>{
-    console.log('server is Rocking on')
-})
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log("server is Rocking on");
+});
